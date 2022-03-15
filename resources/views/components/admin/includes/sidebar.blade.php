8d16d4fbd917bcfde80029114f2ad11879320be6
Hossam Tarek <hossamtarek250@gmail.com>
2022-03-15 13:11:27 +0200

    Autoload helpers file

--- resources/views/components/admin/includes/sidebar.blade.php
+++ resources/views/components/admin/includes/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('admin.home') }}" class="brand-link">
-        <img src="{{ asset('admin-assets/dist/img/AdminLTELogo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{ asset(getSetting('logo')) }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span>
     </a>
 
